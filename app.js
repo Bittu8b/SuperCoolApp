3af515ffff42270ebda0303d702197e136610f0e
Saubhik Singh <gsc-30349@GSLAB.COM>
2019-03-13 23:52:04 +0530

    Working

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const http = require("http");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-
-  res.setHeader("Content-Type", "text/html");
-
-  res.end("<h1>Hello World</h1>");
+app.get("/getUser", (request, response) => {
+  response.json({ name: "Saubhik Singj" });
 });
 
-server.listen(port, () => {
-  console.log(`Server running at port ` + port);
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+
+//   res.setHeader("Content-Type", "text/html");
+
+//   res.end("<h1>Hello World</h1>");
+// });
+
+// server.listen(port, () => {
+//   console.log(`Server running at port ` + port);
+// });
+
+app.listen(port, () => {
+  console.log("Listenng on port " + port);
 });
